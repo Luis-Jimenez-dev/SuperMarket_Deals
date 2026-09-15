75ced3831887c592e375748fa51dcce68a9f49dc
Luis Jimenez <66438439+Luis-Jimenez-dev@users.noreply.github.com>
2026-09-14 17:34:01 -0700

    Update Nob Hill deal import
    Switches the import script from Safeway data to Nob Hill weekly deal data and adds a new weekly_ads row for the 2026-09-09 to 2026-09-15 ad period. The old direct deal-item insertion flow is left commented out while the script is being updated to use the weekly ad ID generated from the new ad record.

--- import_deals.py
+++ import_deals.py
@@ -1,17 +1,41 @@
 import json
 import database
 
-with open ("data/safeway/weekly_deals.json", 'r', encoding='utf-8') as file:
+with open ("data/nob_hill/weekly_deals.json", 'r', encoding='utf-8') as file:
     deals = json.load(file)
 
-for deal in deals:
-    database.cur.execute (
-        """
-        INSERT INTO deal_items (weekly_ad_id, item, unit, category, price)
-        VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT DO NOTHING
-        """,
-        (1, deal['item'], deal['unit'], deal['category'], deal ['price'])
-    )
+database.cur.execute(
+    """
+    INSERT INTO weekly_ads (store_id, start_date, end_date)
+    VALUES (%s, %s, %s)
+    """,
+    (3, '2026-09-09', '2026-09-15'),
+)
 
 database.conn.commit()
+
+# store_name = "Nob Hill"
+
+# database.cur.execute(
+#     """
+#     SELECT weekly_ads.id FROM weekly_ads JOIN stores ON weekly_ads.store_id = stores.id WHERE stores.name ILIKE %s
+#     """,
+#     (store_name,)
+# )
+
+# weekly_ad_id = database.cur.fetchone()
+# weekly_ad_id = weekly_ad_id['id']
+
+# print (weekly_ad_id)
+
+# for deal in deals:
+#     database.cur.execute (
+#         """
+#         INSERT INTO deal_items (weekly_ad_id, item, unit, category, price)
+#         VALUES (%s, %s, %s, %s, %s)
+#         ON CONFLICT DO NOTHING
+#         """,
+#         (weekly_ad_id, deal['item'], deal['unit'], deal['category'], deal ['price'])
+#     )
+
+# database.conn.commit()
